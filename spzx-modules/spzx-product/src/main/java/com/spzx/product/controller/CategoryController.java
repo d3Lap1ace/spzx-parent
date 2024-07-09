bd41bd7ac6c3feaa99cbf61ebc10966849466fd8
d3lap1ace L.Lyu <ap1cem@163.com>
2024-07-09 19:46:49 +0800

    数据导入 debug v0.3.3.1

--- spzx-modules/spzx-product/src/main/java/com/spzx/product/controller/CategoryController.java
+++ spzx-modules/spzx-product/src/main/java/com/spzx/product/controller/CategoryController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
