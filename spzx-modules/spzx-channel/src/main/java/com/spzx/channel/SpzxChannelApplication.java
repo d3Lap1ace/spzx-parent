b7ef7131b306a176745ea18f5ec39eb28dc53640
d3lap1ace L.Lyu <ap1cem@163.com>
2024-07-16 19:21:37 +0800

    bloomfilter

--- spzx-modules/spzx-channel/src/main/java/com/spzx/channel/SpzxChannelApplication.java
+++ spzx-modules/spzx-channel/src/main/java/com/spzx/channel/SpzxChannelApplication.java
@@ -2,6 +2,9 @@ package com.spzx.channel;
 
 import com.spzx.common.security.annotation.EnableCustomConfig;
 import com.spzx.common.security.annotation.EnableRyFeignClients;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -13,4 +16,5 @@ public class SpzxChannelApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpzxChannelApplication.class);
     }
+
 }
--- spzx-modules/spzx-product/src/main/java/com/spzx/product/SpzxProductApplication.java
+++ spzx-modules/spzx-product/src/main/java/com/spzx/product/SpzxProductApplication.java
@@ -1,11 +1,19 @@
 package com.spzx.product;
 
+import com.spzx.product.api.domain.ProductSku;
+import com.spzx.product.mapper.ProductSkuMapper;
+import jakarta.annotation.Resource;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.spzx.common.security.annotation.EnableCustomConfig;
 import com.spzx.common.security.annotation.EnableRyFeignClients;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 商品模块
@@ -15,8 +23,14 @@ import java.io.IOException;
 @EnableCustomConfig
 @EnableRyFeignClients
 @SpringBootApplication
-public class SpzxProductApplication
-{
+public class SpzxProductApplication implements CommandLineRunner {
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Resource
+    private ProductSkuMapper productSkuMapper;
+
     public static void main(String[] args)
     {
         SpringApplication.run(SpzxProductApplication.class, args);
@@ -58,5 +72,18 @@ public class SpzxProductApplication
                 " ———-'\\'————_-•´\n" +
                 " — ———\\———--(\n" +
                 " —— ——-\\———-");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //初始化布隆过滤器
+        RBloomFilter<Object> bloomFilter = redissonClient.getBloomFilter("sku:bloom:filter");
+        //设置数据规模 误判率 预计统计元素数量为100000，期望误差率为0.01
+        bloomFilter.tryInit(100000,0.01);
+        //测试使用，快速自动加入
+        List<ProductSku> list = productSkuMapper.selectProductSkuList(null);
+        list.forEach(it->{
+            bloomFilter.add(it.getId());
+        });
     }
 }
